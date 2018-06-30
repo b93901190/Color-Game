8badd01d7e2a70abbd333c6fc7701bfe30a7d3c1
TSUNG-CHUAN MA <cutekiawe@gmail.com>
2018-06-29 22:46:14 -0700

    add hover and transition effect
    add border-radius
    --webkit-transition
    -moz-transition

--- colorgame.js
+++ colorgame.js
@@ -51,7 +51,9 @@ resetButton.addEventListener("click", function(){
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
+    messageDisplay.textContent = "";
+    this.textContent = "New Colors";
 });
 
 colorDisplay.textContent = pickedColor;
