41389fe2fffdcd1feb628e6ef7cfe6aa4d07a052
TSUNG-CHUAN MA <cutekiawe@gmail.com>
2018-06-29 18:52:02 -0700

    fix bug when switch between easy and hard mode

--- colorgame.js
+++ colorgame.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColor(6);
+var numOfSquare = 6;
+var colors = generateRandomColor(numOfSquare);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -6,10 +7,43 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numOfSquare = 3;
+    colors = generateRandomColor(numOfSquare);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i<squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else{
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");   
+    numOfSquare = 6; 
+    colors = generateRandomColor(numOfSquare);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i<squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
 
 resetButton.addEventListener("click", function(){
     //gen color
-    colors = generateRandomColor(6);
+    colors = generateRandomColor(numOfSquare);
     //pick a new random color from array
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
